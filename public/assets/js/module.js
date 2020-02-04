a400691196cd381582029582f8c98a73e1585143
yesmancan <yesmancan_0611@hotmail.com>
2020-02-04 23:11:08 +0300

    Update module.js

--- public/assets/js/module.js
+++ public/assets/js/module.js
@@ -28,11 +28,19 @@ const navActiveStatus = () => {
     const link = document.querySelectorAll('.sidebar-wrapper .nav.main-nav > li')
     for (let i = 0; i < link.length; i++) {
         const element = link[i];
+        const href = element.querySelector('a').getAttribute('href');
+        console.log()
         element.classList.remove('active');
-        if (element.querySelector('a').getAttribute('href') === path) {
+        if (href === path) {
             element.classList.add('active');
+        } else if (path.includes(href)) {
+            const arrHref = path.split('/')
+            if (arrHref.length > 0) {
+                if ('/' + arrHref[1] === href) {
+                    element.classList.add('active');
+                }
+            }
         }
-
     }
 }
 
